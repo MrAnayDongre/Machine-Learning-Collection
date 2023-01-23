9cd2ffc120ac9414baeddf95c07179fcde4eb641
Anay Dongre <dongreanay@gmail.com>
2023-01-23 20:15:38 +0530

    update

--- ML/PyTorch/NanoGPT/v2.py
+++ ML/PyTorch/NanoGPT/v2.py
@@ -18,8 +18,7 @@ dropout = 0.2
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('WikiQA-train.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
